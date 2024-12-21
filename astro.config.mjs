56ab2d277f37d04ad9061c27e7e260300d2920cb
VOWAC <yukihakanai.2@gmail.com>
2024-12-21 20:43:35 +0900

    install @astro/vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 
 export default defineConfig({
-
+  output: 'server',
+  adapter: vercelServerless({}),
 });
